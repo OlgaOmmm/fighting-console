d9425c56fa11493517c1ce1af40040652d4042f4
Olga <53094440+OlgaOmmm@users.noreply.github.com>
2019-08-26 20:58:37 +0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -10,5 +10,8 @@ public:
 	char getPlayerData(const char& diapason);
 	bool isEndOfGame(const std::unique_ptr<Hero>& player1, const std::unique_ptr<Hero>& player2);
 
+	void playersCreating(std::unique_ptr<Hero>(& player)[2]);
+	void playersOutfit(std::unique_ptr<Hero>(&player)[2]);
+
 	friend std::ostream& operator<<(std::ostream& os, const std::unique_ptr<Hero>& hero);
 };
