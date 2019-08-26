9cabe3a26657c0805812c1a0026ec7afedb478e5
Olga <53094440+OlgaOmmm@users.noreply.github.com>
2019-08-26 19:22:35 +0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <iostream>
+
 #include "Hero.h"
 
 class Game {
 public:
-	Game();
+	explicit Game();
 	void start();
 	char getPlayerData(const char& diapason);
-	bool endOfGame(const std::unique_ptr<Hero>& player1, const std::unique_ptr<Hero>& player2);
+	bool isEndOfGame(const std::unique_ptr<Hero>& player1, const std::unique_ptr<Hero>& player2);
+
+	friend std::ostream& operator<<(std::ostream& os, const std::unique_ptr<Hero>& hero);
 };
