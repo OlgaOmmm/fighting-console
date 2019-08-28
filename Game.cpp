b0977e7c813458c862e4392bc9a3fde1698b120e
Olga <53094440+OlgaOmmm@users.noreply.github.com>
2019-08-28 10:14:27 +0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -14,7 +14,6 @@ Game::Game() {
 };
 
 void Game::start() {
-	// ------------------------ CREATING ------------------------
 	std::unique_ptr<Hero> player[2];
 	playersCreating(player);
 
@@ -23,110 +22,8 @@ void Game::start() {
 	std::cout << "\n\tPlayer1 said:\n" << player[0];
 	std::cout << "\n\tPlayer2 said:\n" << player[1];
 
-	//------------------------ AUTO FIGHT ------------------------
-
 	std::cout << "\n\tFIGHT!!!\n\n";
-	
-	while (true) {
-		if (player[0]->Spd >= player[1]->Spd) {
-			player[0]->attack(player[0], player[1]);
-			if (!isEndOfGame(player[0], player[1]))
-				player[1]->attack(player[1], player[0]);
-			else
-				break;
-		}
-		else
-		{
-			player[1]->attack(player[1], player[0]);
-			if (!isEndOfGame(player[0], player[1]))
-				player[0]->attack(player[0], player[1]);
-			else
-				break;
-		}
-	}
-}
-
-void Game::playersOutfit(std::unique_ptr<Hero>(&player)[2]) {
-	for (int i = 0; i < 2; ++i) {
-		std::cout << "\t\tPlease, choose outfit for player" << i + 1 << "\n";
-
-		//right hand (weapon)
-		std::unique_ptr<Weapon> playerWeapon[2];
-		std::cout << "\tWeapon in right hand: 1-Sword, 2-Katana, 3-Knife, 4-None\t";
-		switch (getPlayerData('4'))
-		{
-		case '1': playerWeapon[i] = std::make_unique<Sword>(player[i]); break;
-		case '2': playerWeapon[i] = std::make_unique<Katana>(player[i]); break;
-		case '3': playerWeapon[i] = std::make_unique<Knife>(player[i]); break;
-		case '4': playerWeapon[i] = std::make_unique<NoWeapon>(player[i]); break;
-		}
-
-		//left hand (shield or weapon)
-		std::unique_ptr<Weapon2> playerWeapon2[2];
-		std::cout << "\tIn left hand: 1-Shield, 2-Sword, 3-Knife, 4-None\t";
-		switch (getPlayerData('4'))
-		{
-		case '1': playerWeapon2[i] = std::make_unique<Shield>(player[i]); break;
-		case '2': playerWeapon2[i] = std::make_unique<Sword2>(player[i]); break;
-		case '3': playerWeapon2[i] = std::make_unique<Knife2>(player[i]); break;
-		case '4': playerWeapon2[i] = std::make_unique<NoWeapon2>(player[i]); break;
-		}
-
-		//armor
-		std::unique_ptr<Armor> playerArmor[2];
-		std::cout << "\tArmor: 1-Steel, 2-Plate, 3-Leather, 4-None\t";
-		switch (getPlayerData('4'))
-		{
-		case '1': playerArmor[i] = std::make_unique<SteelArmor>(player[i]); break;
-		case '2': playerArmor[i] = std::make_unique<PlateArmor>(player[i]); break;
-		case '3': playerArmor[i] = std::make_unique<LeatherArmor>(player[i]); break;
-		case '4': playerArmor[i] = std::make_unique<NoArmor>(player[i]); break;
-		}
-
-		//helmet
-		std::unique_ptr<Helmet> playerHelmet[2];
-		std::cout << "\tHelmet: 1-Steel, 2-Plate, 3-Leather, 4-None\t";
-		switch (getPlayerData('4'))
-		{
-		case '1': playerHelmet[i] = std::make_unique<SteelHelmet>(player[i]); break;
-		case '2': playerHelmet[i] = std::make_unique<PlateHelmet>(player[i]); break;
-		case '3': playerHelmet[i] = std::make_unique<LeatherHelmet>(player[i]); break;
-		case '4': playerHelmet[i] = std::make_unique<NoHelmet>(player[i]); break;
-		}
-
-		//gloves
-		std::unique_ptr<Gloves> playerGloves[2];
-		std::cout << "\tGloves: 1-Steel, 2-Plate, 3-Leather, 4-None\t";
-		switch (getPlayerData('4'))
-		{
-		case '1': playerGloves[i] = std::make_unique<SteelGloves>(player[i]); break;
-		case '2': playerGloves[i] = std::make_unique<PlateGloves>(player[i]); break;
-		case '3': playerGloves[i] = std::make_unique<LeatherGloves>(player[i]); break;
-		case '4': playerGloves[i] = std::make_unique<NoGloves>(player[i]); break;
-		}
-
-		//greaves
-		std::unique_ptr<Greaves> playerGreaves[2];
-		std::cout << "\tGreaves: 1-Steel, 2-Plate, 3-Leather, 4-None\t";
-		switch (getPlayerData('4'))
-		{
-		case '1': playerGreaves[i] = std::make_unique<SteelGreaves>(player[i]); break;
-		case '2': playerGreaves[i] = std::make_unique<PlateGreaves>(player[i]); break;
-		case '3': playerGreaves[i] = std::make_unique<LeatherGreaves>(player[i]); break;
-		case '4': playerGreaves[i] = std::make_unique<NoGreaves>(player[i]); break;
-		}
-
-		//ring
-		std::unique_ptr<Ring> playerRing[2];
-		std::cout << "\tRing: 1-Emerald, 2-Ruby, 3-Saphire, 4-None\t";
-		switch (getPlayerData('4'))
-		{
-		case '1': playerRing[i] = std::make_unique<EmeraldRing>(player[i]); break;
-		case '2': playerRing[i] = std::make_unique<RubyRing>(player[i]); break;
-		case '3': playerRing[i] = std::make_unique<SaphireRing>(player[i]); break;
-		case '4': playerRing[i] = std::make_unique<NoRing>(player[i]); break;
-		}
-	}
+	autoFight(player);
 }
 
 void Game::playersCreating(std::unique_ptr<Hero>(&player)[2]) {
@@ -145,7 +42,102 @@ void Game::playersCreating(std::unique_ptr<Hero>(&player)[2]) {
 	}
 }
 
-bool Game::isEndOfGame(const std::unique_ptr<Hero>& player1, const std::unique_ptr<Hero>& player2) {
+void Game::playersOutfit(const std::unique_ptr<Hero>(&player)[2]) const {
+	for (int i = 0; i < 2; ++i) {
+		std::cout << "\t\tPlease, choose outfit for player" << i + 1 << "\n";
+
+		std::unique_ptr<Weapon> playerWeapon[2];
+		std::cout << "\tWeapon in right hand: 1-Sword, 2-Katana, 3-Knife, 4-None\t";
+		switch (getPlayerData('4')) {
+		case '1': playerWeapon[i] = std::make_unique<Sword>(player[i]); break;
+		case '2': playerWeapon[i] = std::make_unique<Katana>(player[i]); break;
+		case '3': playerWeapon[i] = std::make_unique<Knife>(player[i]); break;
+		case '4': playerWeapon[i] = std::make_unique<NoWeapon>(player[i]); break;
+		}
+
+		std::unique_ptr<Weapon2> playerWeapon2[2];
+		std::cout << "\tIn left hand: 1-Shield, 2-Sword, 3-Knife, 4-None\t";
+		switch (getPlayerData('4')) {
+		case '1': playerWeapon2[i] = std::make_unique<Shield>(player[i]); break;
+		case '2': playerWeapon2[i] = std::make_unique<Sword2>(player[i]); break;
+		case '3': playerWeapon2[i] = std::make_unique<Knife2>(player[i]); break;
+		case '4': playerWeapon2[i] = std::make_unique<NoWeapon2>(player[i]); break;
+		}
+
+		std::unique_ptr<Armor> playerArmor[2];
+		std::cout << "\tArmor: 1-Steel, 2-Plate, 3-Leather, 4-None\t";
+		switch (getPlayerData('4')) {
+		case '1': playerArmor[i] = std::make_unique<SteelArmor>(player[i]); break;
+		case '2': playerArmor[i] = std::make_unique<PlateArmor>(player[i]); break;
+		case '3': playerArmor[i] = std::make_unique<LeatherArmor>(player[i]); break;
+		case '4': playerArmor[i] = std::make_unique<NoArmor>(player[i]); break;
+		}
+
+		std::unique_ptr<Helmet> playerHelmet[2];
+		std::cout << "\tHelmet: 1-Steel, 2-Plate, 3-Leather, 4-None\t";
+		switch (getPlayerData('4')) {
+		case '1': playerHelmet[i] = std::make_unique<SteelHelmet>(player[i]); break;
+		case '2': playerHelmet[i] = std::make_unique<PlateHelmet>(player[i]); break;
+		case '3': playerHelmet[i] = std::make_unique<LeatherHelmet>(player[i]); break;
+		case '4': playerHelmet[i] = std::make_unique<NoHelmet>(player[i]); break;
+		}
+
+		std::unique_ptr<Gloves> playerGloves[2];
+		std::cout << "\tGloves: 1-Steel, 2-Plate, 3-Leather, 4-None\t";
+		switch (getPlayerData('4')) {
+		case '1': playerGloves[i] = std::make_unique<SteelGloves>(player[i]); break;
+		case '2': playerGloves[i] = std::make_unique<PlateGloves>(player[i]); break;
+		case '3': playerGloves[i] = std::make_unique<LeatherGloves>(player[i]); break;
+		case '4': playerGloves[i] = std::make_unique<NoGloves>(player[i]); break;
+		}
+
+		std::unique_ptr<Greaves> playerGreaves[2];
+		std::cout << "\tGreaves: 1-Steel, 2-Plate, 3-Leather, 4-None\t";
+		switch (getPlayerData('4')) {
+		case '1': playerGreaves[i] = std::make_unique<SteelGreaves>(player[i]); break;
+		case '2': playerGreaves[i] = std::make_unique<PlateGreaves>(player[i]); break;
+		case '3': playerGreaves[i] = std::make_unique<LeatherGreaves>(player[i]); break;
+		case '4': playerGreaves[i] = std::make_unique<NoGreaves>(player[i]); break;
+		}
+
+		std::unique_ptr<Ring> playerRing[2];
+		std::cout << "\tRing: 1-Emerald, 2-Ruby, 3-Saphire, 4-None\t";
+		switch (getPlayerData('4')) {
+		case '1': playerRing[i] = std::make_unique<EmeraldRing>(player[i]); break;
+		case '2': playerRing[i] = std::make_unique<RubyRing>(player[i]); break;
+		case '3': playerRing[i] = std::make_unique<SaphireRing>(player[i]); break;
+		case '4': playerRing[i] = std::make_unique<NoRing>(player[i]); break;
+		}
+	}
+}
+
+void Game::autoFight(const std::unique_ptr<Hero>(&player)[2]) const {
+	while (true) {
+		if (player[0]->Spd >= player[1]->Spd) {
+			if (!isEndOfGame(player[0], player[1]))
+				player[0]->attack(player[0], player[1]);
+			else
+				break;
+			if (!isEndOfGame(player[0], player[1]))
+				player[1]->attack(player[1], player[0]);
+			else
+				break;
+		}
+		else
+		{
+			if (!isEndOfGame(player[0], player[1]))
+				player[0]->attack(player[1], player[0]);
+			else
+				break;
+			if (!isEndOfGame(player[0], player[1]))
+				player[1]->attack(player[0], player[1]);
+			else
+				break;
+		}
+	}
+}
+
+bool Game::isEndOfGame(const std::unique_ptr<Hero>& player1, const std::unique_ptr<Hero>& player2) const {
 	if (player1->HP == 0) {
 		std::cout << player2->Name << " win!!!\n\nGAME OVER\n";
 		return true;
@@ -158,7 +150,7 @@ bool Game::isEndOfGame(const std::unique_ptr<Hero>& player1, const std::unique_p
 		return false;
 }
 
-char Game::getPlayerData(const char& diapason) {
+char Game::getPlayerData(const char& diapason) const {
 	std::string playerData;
 	char verifiedData;
 	while (true) {
